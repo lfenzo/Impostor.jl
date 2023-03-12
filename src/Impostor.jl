5b8edafe2e3c9f9642b8dea41b799c3a0823bb89
lfenzo <lfenzo@protonmail.com>
2023-03-12 15:36:30 -0300

    feat: added basic JSON io from the predefined asset structure

--- src/Impostor.jl
+++ src/Impostor.jl
@@ -5,11 +5,35 @@ using Distributions
 
 include("core/loading.jl")
 include("core/impostor.jl")
+include("core/data_container.jl")
+
+include("providers/base.jl")
+include("providers/identity.jl")
+
+
+
 
 
 function main()
     imp = Impostor(["en_US", "pt_BR"]; providers = [:ae_porra])
-    println(rand(imp, 30))
+
+    container = DataContainer()
+#    println(haslocale(container, "en_US"))
+#    println(hasprovider(container, "en_US", "identity"))
+#    println(hascontent(container, "en_US", "identity", :name))
+
+    println("....................")
+    a = load!(container; locale = "en_US", provider = "identity", content = "name")
+
+
+    println("test ", haslocale(container, "en_US"))
+    println("test ", hasprovider(container, "en_US", "identity"))
+    println("test ", hascontent(container, "en_US", "identity", "name"))
+
+    return a
+    #rand(names([:prefix, :firstname, :surname]; locale = "pt_BR"), 200) |> println
+
+    #println(rand(imp, 30))
 end
 
 
