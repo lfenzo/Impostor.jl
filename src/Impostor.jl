ca6ad1d8e03a652b0636b3e3f65ce6b946e39530
lfenzo <lfenzo@protonmail.com>
2023-03-17 19:45:32 -0300

    feat: introduced conditional and requirement-based loading

--- src/Impostor.jl
+++ src/Impostor.jl
@@ -1,30 +1,23 @@
 using JSON3
 using Random
-using Distributions
+using Base.Iterators
 
 
-include("core/impostor.jl")
-include("core/data_container.jl")
+include("core/random_selection.jl")
+include("core/data_interface.jl")
 
-include("providers/base.jl")
+#include("providers/base.jl")
 include("providers/identity.jl")
 
 
 container::DataContainer = DataContainer()
 
 
+
 function main()
-
-    println("....................")
-    #a = load!(container; locale = "en_US", provider = "identity", content = "names")
-
-    println("test ", haslocale(container, "en_US"))
-    println("test ", hasprovider(container, "en_US", "identity"))
-    println("test ", hascontent(container, "en_US", "identity", "name"))
-
-    rand(names([:firstname]; sex = "both"), 200) |> println
-
-    #println(rand(imp, 30))
+    println(firstname("male", 7))
+    println(load!(["male", "male", "female", "female"], "firstnames", "identity", ["en_US"]))
+    println(surname(7))
 end
 
 
