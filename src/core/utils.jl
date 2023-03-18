f0b6dfa6665858d5c238df80c999b4df6286ec54
lfenzo <lfenzo@protonmail.com>
2023-03-18 13:01:34 -0300

    feat: implemented some identity related random generators

---
+++ src/core/utils.jl
@@ -0,0 +1,9 @@
+"""
+
+"""
+function random_date(n::Int; start::Date = Date(1900, 1, 1), stop::Date = today()) :: Vector{Date}
+    return rand(start:Day(1):stop, n)
+end
+
+
+function regex2string() end
