9aa322bd689056be2f471563cfc4c7ceb20d04cf
lfenzo <lfenzo@protonmail.com>
2023-03-25 16:23:59 -0300

    test: added more tests to the data interface and identity provider

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,8 @@ using Test
 using JSON3
 
 
-include("test_utils.jl")
+include("utils.jl")
+include(joinpath(pkgdir(Impostor), "src", "providers", "relation_restrictions.jl"))
 
 
 # common aliases to test 
@@ -11,10 +12,14 @@ const OPTIONLESS::String = "Random Values (neither Options nor Mask)"
 const MASK_LOADING::String = "Mask-Based Loading"
 const OPTION_LOADING::String = "Option-Based Loading"
 
+# list of all available locales to be checked for intetritty
+const ALL_LOCALES::Vector{String} = [d for d in readdir(pkgdir(Impostor, "src", "data")) if d != "template"]
+
 
 testsets = Dict{String, String}(
-   "Data Interface" => "data_interface.jl",
-   "Identity" => "identity.jl",
+   "Identity" => "test_identity.jl",
+   "Data Interface" => "test_data_interface.jl",
+   "Data Integrity" => "test_data_integrity.jl",
 )
 
 @testset "Impostor" begin
